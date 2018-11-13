cd305e9a4b5183fae301251ff06c88efcce7fd95
sky0621 <sky14work@gmail.com>
2018-11-13 09:50:24 +0900

    appengine

--- backend/server.go
+++ backend/server.go
@@ -13,5 +13,20 @@ func main() {
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "[environment] is app engine: %v, is dev: %v\n", appengine.IsAppEngine(), appengine.IsDevAppServer())
+	fmt.Fprintln(w, "Hello, GAE/go")
+	c := appengine.NewContext(r)
+	fmt.Fprintf(w, "[AppID]: %v\n", appengine.AppID(c))
+	fmt.Fprintf(w, "[Datacenter]: %v\n", appengine.Datacenter(c))
+	fmt.Fprintf(w, "[DefaultVersionHostname]: %v\n", appengine.DefaultVersionHostname(c))
+	fmt.Fprintf(w, "[InstanceID]: %v\n", appengine.InstanceID())
+	fmt.Fprintf(w, "[IsAppEngine]: %v\n", appengine.IsAppEngine())
+	fmt.Fprintf(w, "[IsDevAppServer]: %v\n", appengine.IsDevAppServer())
+	fmt.Fprintf(w, "[IsFlex]: %v\n", appengine.IsFlex())
+	fmt.Fprintf(w, "[IsStandard]: %v\n", appengine.IsStandard())
+	fmt.Fprintf(w, "[ModuleName]: %v\n", appengine.ModuleName(c))
+	fmt.Fprintf(w, "[RequestID]: %v\n", appengine.RequestID(c))
+	fmt.Fprintf(w, "[ServerSoftware]: %v\n", appengine.ServerSoftware())
+	serviceAccount, _ := appengine.ServiceAccount(c)
+	fmt.Fprintf(w, "[ServiceAccount]: %v\n", serviceAccount)
+	fmt.Fprintf(w, "[VersionID]: %v\n", appengine.VersionID(c))
 }
