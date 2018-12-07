cebbf1bbecff2df40789375949c3a52c5253887e
sky0621 <sky14work@gmail.com>
2018-12-07 09:25:11 +0900

    fin

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -7,9 +7,14 @@ import (
 	"go-webapi-for-gae-study/backend/model"
 	"google.golang.org/appengine"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main()  {
+	// この接続情報は、あくまでローカル（docker-composeで立ち上げるMySQLへの接続限定）用
+	// GAE環境にデプロイする時は、接続情報はもとより、形式も下記のように変える必要がある。
+	// "【ユーザ】:【パスワード】@unix(/cloudsql/【インスタンス接続文字列（※おそらく「【プロジェクト名】:【リージョン】:【インスタンスID】」）】)/【DB名】"
 	db, err := gorm.Open("mysql", "testuser:testpass@tcp(127.0.0.1:3306)/testdb?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
