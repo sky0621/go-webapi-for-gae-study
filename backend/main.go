775dc569380cf013d86362816a54bca2c7df508c
sky0621 <sky14work@gmail.com>
2018-12-09 22:35:19 +0900

    local or gae

--- backend/main.go
+++ backend/main.go
@@ -1,21 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
 	"go-webapi-for-gae-study/backend/controller"
 	"go-webapi-for-gae-study/backend/model"
 	"google.golang.org/appengine"
 	"net/http"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main()  {
-	// この接続情報は、あくまでローカル（docker-composeで立ち上げるMySQLへの接続限定）用
-	// GAE環境にデプロイする時は、接続情報はもとより、形式も下記のように変える必要がある。
-	// "【ユーザ】:【パスワード】@unix(/cloudsql/【インスタンス接続文字列（※おそらく「【プロジェクト名】:【リージョン】:【インスタンスID】」）】)/【DB名】"
-	db, err := gorm.Open("mysql", "testuser:testpass@tcp(127.0.0.1:3306)/testdb?charset=utf8&parseTime=True&loc=Local")
+	var (
+		connectionName = os.Getenv("CLOUDSQL_CONNECTION_NAME")
+		user           = os.Getenv("CLOUDSQL_USER")
+		password       = os.Getenv("CLOUDSQL_PASSWORD")
+		database       = os.Getenv("CLOUDSQL_DATABASE")
+	)
+	db, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@unix(/cloudsql/%s)/%s", user, password, connectionName, database))
+	if appengine.IsDevAppServer() {
+		db, err = gorm.Open("mysql", "testuser:testpass@tcp(127.0.0.1:3306)/testdb?charset=utf8&parseTime=True&loc=Local")
+	}
 	if err != nil {
 		panic(err)
 	}
