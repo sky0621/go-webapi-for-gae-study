bd78bb5f5a91dbb861ea78f4d8914ece81fb3dba
sky0621 <sky14work@gmail.com>
2019-04-01 02:03:27 +0900

    continue

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"net/http"
 	"os"
-	"time"
 
 	"github.com/google/uuid"
 
@@ -14,21 +14,26 @@ import (
 )
 
 func main() {
-	var (
-		isLocal = os.Getenv("IS_LOCAL")
-	)
+	// --------------------------------------------------------------
+	// GCP - CloudSQL への接続情報は環境変数から取得
+	// --------------------------------------------------------------
 	var (
 		connectionName = os.Getenv("CLOUDSQL_CONNECTION_NAME")
 		user           = os.Getenv("CLOUDSQL_USER")
 		password       = os.Getenv("CLOUDSQL_PASSWORD")
 		database       = os.Getenv("CLOUDSQL_DATABASE")
 	)
-
 	dataSource := fmt.Sprintf("%s:%s@unix(/cloudsql/%s)/%s?parseTime=True", user, password, connectionName, database)
-	if isLocal != "" {
+
+	// ローカル環境（docker-composeでMySQLを起動）での動作確認用
+	isLocal := os.Getenv("IS_LOCAL") != ""
+	if isLocal {
 		dataSource = "testuser:testpass@tcp(localhost:3306)/testdb?charset=utf8&parseTime=True&loc=Local"
 	}
 
+	// --------------------------------------------------------------
+	// GCP - CloudSQL へ接続
+	// --------------------------------------------------------------
 	db, err := sql.Open("mysql", dataSource)
 	if err != nil {
 		panic(err)
@@ -42,38 +47,41 @@ func main() {
 		}
 	}()
 
-	db.SetConnMaxLifetime(60 * time.Second)
-	db.SetMaxIdleConns(10)
-	db.SetMaxOpenConns(10)
-	if err := db.Ping(); err != nil {
-		panic(err)
-	}
+	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("[GWFGS] /users")
+		switch r.Method {
+		case http.MethodPost:
+			fmt.Println("[GWFGS] POST")
+			if err := r.ParseForm(); err != nil {
+				if _, err := fmt.Fprintln(w, err.Error()); err != nil {
+					panic(err)
+				}
+			}
+			form := r.PostForm
+			fmt.Printf("[GWFGS] PostForm: %#v\n", form)
+			name := form.Get("name")
+			fmt.Printf("[GWFGS] PostForm[name]: %s\n", name)
+			mail := form.Get("mail")
+			fmt.Printf("[GWFGS] PostForm[mail]: %s\n", mail)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer func() {
-		if cancel != nil {
-			cancel()
+			result, err := db.ExecContext(context.Background(), "INSERT INTO user(id, name, mail, create_user) VALUES(?, ?, ?, ?)",
+				uuid.New().String(), name, mail, "admin")
+			if err != nil {
+				if _, err := fmt.Fprintln(w, err.Error()); err != nil {
+					panic(err)
+				}
+			}
+			if result == nil {
+				panic(errors.New("result is nil"))
+			}
 		}
-	}()
+	})
 
-	result, err := db.ExecContext(ctx, "INSERT INTO user(id, name, mail, create_user) VALUES(?, ?, ?, ?)",
-		uuid.New().String(), "Taro", "taro@example.com", "dummy")
-	if err != nil {
+	addr := ":80"
+	if isLocal {
+		addr = ":8080"
+	}
+	if err := http.ListenAndServe(addr, nil); err != nil {
 		panic(err)
 	}
-	if result == nil {
-		panic(errors.New("result is nil"))
-	}
-
-	id, err := result.LastInsertId()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("LastInsertId: %d\n", id)
-
-	rows, err := result.RowsAffected()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("RowsAffected: %d\n", rows)
 }
